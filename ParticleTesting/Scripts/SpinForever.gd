934479fd81f4418b6ab16aadcb478340c7371b4e
sourencho <souren@papazian.info>
2021-10-02 22:15:39 -0700

    dont spin forever

--- ParticleTesting/Scripts/SpinForever.gd
+++ ParticleTesting/Scripts/SpinForever.gd
@@ -13,5 +13,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.rotation_degrees += 1
+	# self.rotation_degrees += 1
 	pass
